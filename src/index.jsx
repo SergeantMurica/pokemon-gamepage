bff494dc5b8c6d13009bedced4a993b97efe53c3
SergeantMurica <Iwontforgetureena@hotmail.com>
2025-01-02 22:05:02 -0700

    Fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+>
+    <App/>
+</DevSupport>);
